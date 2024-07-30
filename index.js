29188e466e44275295f9eff13b90270b38e353d4
Lokesh Mohan Mahankali <lokeshmahankali@gmail.com>
2024-07-30 23:14:21 +0530

    cred

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
-dotenv.config();
+const userRoute = require("./routes/user");
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Databaseconncetion is successfull"))
   .catch((err) => console.log(err));
+
+// userroute
+app.use(express.json());
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port 5000");
