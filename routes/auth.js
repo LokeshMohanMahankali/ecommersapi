efe01d426d7797f2e053b553bd012f938d90c3f7
Lokesh Mohan Mahankali <lokeshmahankali@gmail.com>
2024-07-31 15:01:30 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
   });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
